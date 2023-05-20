139df0e1bcf779a4b2b6959532b5743465672ef4
risりす <surisuririsu@gmail.com>
2023-05-20 10:09:05 -0400

    Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,20 @@ export default function Home() {
     <>
       <Head>
         <title>risりす</title>
-        <meta name="description" content="I make weeb art" />
+        <meta name="author" content="risりす" />
+        <meta name="description" content="I make weeb art. Links to my socials." />
+        <meta name="keywords" content="Art, Anime, Illustration, surisuririsu" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="og:title" content="risりす" />
+        <meta name="og:url" content="http://surisuririsu.com/" />
+        <meta name="og:description" content="I make weeb art. Links to my socials." />
+        <meta name="og:image" content="https://surisuririsu.com/avatar.jpg" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@surisuririsu" />
+        <meta name="twitter:title" content="risりす" />
+        <meta name="twitter:description" content="I make weeb art. Links to my socials." />
+        <meta name="twitter:image" content="https://surisuririsu.com/avatar.jpg" />
       </Head>
       <main className={styles.main}>
         <div className={`${font.className} ${styles.profile}`}>
